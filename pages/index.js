9298c8997dfa908670f4b4114d4fa5243ebf62a5
Paul Betts <paul@paulbetts.org>
2016-12-08 12:02:43 -0800

    Let's get it started

---
+++ pages/index.js
@@ -0,0 +1,4 @@
+import react from 'react';
+
+export default () => 
+  <h2><a href="slack://channel?team=T024BE7LD&id=C024BEACD">Chat with #team-desktop</a></h2>
---
+++ pages/slackday.js
@@ -0,0 +1,4 @@
+import react from 'react';
+
+export default () => 
+  <h2><a href="slack://channel?team=T024BE7LD&id=C33FDKL1E">Welcome to Slackday!</a></h2>
