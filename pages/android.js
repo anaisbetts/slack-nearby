1b439381b380e8a3bab82da4e3d9cefa87a48cf7
Paul Betts <paul@paulbetts.org>
2016-12-08 13:15:38 -0800

    More hardcoded pages

---
+++ pages/android.js
@@ -0,0 +1,4 @@
+import react from 'react';
+
+export default () => 
+  <h2><a href="slack://channel?team=T024BE7LD&id=C02BNG9V9">Chat with #android-team</a></h2>
---
+++ pages/paulcbetts.js
@@ -0,0 +1,8 @@
+import react from 'react';
+import Head from 'next/head';
+
+export default () => 
+  <div>
+    <Head><title>Hi, I'm Paul.</title></Head>
+    <h2><a href="slack://user?team=T024BE7LD&id=W1M28C0KY">Hi, I'm Paul</a></h2>
+  </div>
